f9e59770aa8ce1162ccf8fef26936347d4418f24
shantanu_dev <122613156+revenger19@users.noreply.github.com>
2024-01-05 20:57:46 +0530

    Basic c++ files

---
+++ learning_tutorials.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int file_size;
+    cout << file_size;
+    return 0;
+}
---
+++ starting_code.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> //Header. This imports all the other headers. Mostly all the necessary funcitons will be imported by this
+using namespace std; // Declaring the namespace std. cin and cout and most of the functions are delcared in this only. 
+
+void solve(){
+
+    // Here we'll solve the problem
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    // The above two lines are reffered to as FAST IO part. As the names suggest, they are used for FAST INPUT AND OUTPUT. Basically it'll make the code run faster. 
+    //int _t;cin>>_t;while(_t--)
+    // This will run the solve, t number of times. It is usefull in cases when you have to run the test case t number of times. 
+    solve();
+}
